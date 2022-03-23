35100db7e0734b4bc98d684f752b8db15a82a331
Praveen <dpraveen453@gmail.com>
2022-03-23 10:48:58 +0530

    commited changes in package

--- src/components/Mentors/Authentication/SignUpPage.js
+++ src/components/Mentors/Authentication/SignUpPage.js
@@ -1,20 +1,22 @@
-import React, { Component, useState, useEffect } from 'react'
+import React, { Component, useState, useEffect, } from 'react'
 import * as EmailValidator from 'email-validator';
 import styles from './SignUpPage.css'
 import {
   Routes, Route, Link, BrowserRouter as Router,
-  Switch,
+  Switch, withRouter
 } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // import db from '../firebaseConfig'
 import db from "../../../firebaseConfig.js"
-
+import MentorSignInPage from './SigninPage.js'
 import { collection, doc, setDoc, addDoc } from "firebase/firestore";
 import { async } from '@firebase/util';
-// const auth = getAuth();
 
-export default function SignInPage() {
 
+
+export default function SignInPage(props) {
+  // let history = useHistory();
   const [name, setname] = useState("")
   const handlename = (e) => {
     setname(e.target.value)
@@ -44,10 +46,13 @@ export default function SignInPage() {
   const [mailerr, setmailerr] = useState(false)
   const [phnoerr, setphnoerr] = useState(false)
   const [passerr, setpasserr] = useState(false)
-  const [validDetails, setvalidDetails] = useState(0)
+  var [validDetails, setvalidDetails] = useState(0)
   const emailAuth = async (name, email, password) => {
+
+    // let navigate = useNavigate();
     try {
       const auth = getAuth();
+
       const res = await createUserWithEmailAndPassword(auth, email, password);
       const user = res.user;
       // const user = userCredential.user;
@@ -60,8 +65,14 @@ export default function SignInPage() {
           email,
           password
         });
-      // test.id = user.uid
       console.log("test");
+      if (test) {
+        console.log("hii routing has done succesfully");
+        // navigate("mentor/signin");
+        // <Route path='*' element={<Navigate to='/mentor/signin' />} />
+
+      }
+
     } catch (err) {
       console.error(err);
       alert(err.message);
@@ -72,6 +83,7 @@ export default function SignInPage() {
     e.preventDefault()
     if (name.length > 6) {
       setnamerr(false)
+      validDetails = 1
       setvalidDetails(1)
     }
     else {
@@ -80,6 +92,8 @@ export default function SignInPage() {
     }
     if (EmailValidator.validate(email)) {
       setmailerr(false)
+      validDetails = 1
+
       setvalidDetails(1)
     }
     else {
@@ -92,6 +106,8 @@ export default function SignInPage() {
     }
     else {
       setphnoerr(false)
+      validDetails = 1
+
       setvalidDetails(1)
     }
     if (Npassword != Cpassword) {
@@ -100,6 +116,8 @@ export default function SignInPage() {
     }
     else {
       setpasserr(false)
+      validDetails = 1
+
       setvalidDetails(1)
     }
     if (validDetails == 1) {
