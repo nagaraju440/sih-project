5ce6713c34ff346163f71be49ee8f491271c41f7
Praveen <dpraveen453@gmail.com>
2022-03-23 12:20:42 +0530

    commited changes in routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react'
 import './App.less';
 import { Button } from 'antd';
 import 'antd/dist/antd.css';
 import AntdSample from './AntdSample/AntdSample';
 import CloudFirestore from './FirebaseSample/CloudFirestore';
-import { Routes, Route, Link, BrowserRouter as Router,
-Switch, } from "react-router-dom";
+import {
+  Routes, Route, Link, BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
 import MentorSignUpPage from './components/Mentors/Authentication/SignUpPage';
 import MentorSignInPage from './components/Mentors/Authentication/SigninPage';
 import StudentSignUpPage from './components/Students/Authentication/SignupPage';
@@ -14,30 +17,32 @@ import MenuBarPage from './components/Dashboard/MenuBarPage';
 import TopNavBar from './components/Dashboard/TopNavBar';
 import Dashboard from './components/Dashboard/Dashboard';
 import LandingPage from './components/LandingPage/LandingPage'
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import MentorDashboard from './components/MentorDashboard/MentorDashboard';
+import Authentication from './Authentication';
 function App() {
+
+  // }
+
+
   return (
-    // <div className="App">
-      // {/* <AntdSample/> */}
-      // <h1>Welcome to React Router!</h1>
-      // <Router>
-          <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="mentor/signup" element={<MentorSignUpPage/>}  />
-        <Route path="mentor/signin" element={<MentorSignInPage/>}  />
-        <Route path="student/signup" element={<StudentSignUpPage/>}  />
-        <Route path="student/signin" element={<StudentSignInPage/>}  />
-        <Route path="about" element={<About />} />
-        <Route path="menuBarPage" element={<MenuBarPage/>}/>
-        <Route path="topnavbar" element={<TopNavBar/>}/>
-        <Route path="dashboard" element={<Dashboard/>}/>
-        <Route path="LandingPage" element={<LandingPage/>}/>
-        
-      </Routes>
-      // {/* </Router> */}
-      // {/* <CloudFirestore/> */}
-     
-    // {/* </div> */}
-  );
+    // <Router>
+    <Routes>
+      <Route path="/" element={<Authentication />} />
+      <Route path="mentor/signup" element={<MentorSignUpPage />} />
+      {/* <Route path="mentor/signup1" element={<SignUpPage1 />} /> */}
+      <Route path="mentor/signin" element={<MentorSignInPage />} />
+      <Route path="student/signup" element={<StudentSignUpPage />} />
+      <Route path="student/signin" element={<StudentSignInPage />} />
+      <Route path="about" element={<About />} />
+      <Route path="menuBarPage" element={<MenuBarPage />} />
+      <Route path="topnavbar" element={<TopNavBar />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="LandingPage" element={<LandingPage />} />
+    </Routes>
+    // </Router>
+  )
+
 }
 
 export default App;
