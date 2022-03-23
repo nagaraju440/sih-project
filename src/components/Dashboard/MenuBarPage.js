e1a06dda26fc72bdc53d61ef662165a154a6cd79
Karanam-Tejaswini <tejaswinikranam5@gamil.com>
2022-03-23 12:57:18 +0530

    dashboard

--- src/components/Dashboard/MenuBarPage.js
+++ src/components/Dashboard/MenuBarPage.js
@@ -1,51 +1,93 @@
 import React from 'react'
-import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent, SidebarFooter } from 'react-pro-sidebar';
+// import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent, SidebarFooter } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 import { Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
 // import './MenuBarPage.css'
 export default function MenuBarPage() {
   return (
     <div>
-        <ProSidebar className='navigation'>
+<Layout>
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+        <Menu.Item key="4" icon={<UserOutlined />}>
+          nav 4
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout>
+      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0' }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>,
+  mountNode,
+        {/* <ProSidebar className='navigation'>
             <SidebarHeader>
                 Header
             </SidebarHeader>
             <SidebarContent>
                 <Menu>
                     <MenuItem>
-                            <Link to="/dashboard">Dashboard</Link>
+                            <Link to="/">Dashboard</Link>
                     </MenuItem>
                     <MenuItem>
-                            <Link to="/dashboard">Profile</Link>
+                            <Link to="/">Profile</Link>
                     </MenuItem>
                     <MenuItem>
-                            <Link to="/dashboard">Courses</Link>
+                            <Link to="/">Courses</Link>
                     </MenuItem>
                     <MenuItem>
-                            <Link to="/dashboard">Assignments</Link>
+                            <Link to="/">Assignments</Link>
                     </MenuItem>
                     <MenuItem>
-                            <Link to="/dashboard">Chat</Link>
+                            <Link to="/">Chat</Link>
                     </MenuItem>
                     <MenuItem>
-                            <Link to="/dashboard">Notifications</Link>
+                            <Link to="/">Notifications</Link>
                     </MenuItem>
                     <MenuItem>
-                            <Link to="/dashboard">Clarify doubts</Link>
+                            <Link to="/">Clarify doubts</Link>
                     </MenuItem>
                     <MenuItem>
-                            <Link to="/dashboard">Add to story</Link>
+                            <Link to="/">Add to story</Link>
                     </MenuItem>
                     <MenuItem>
-                            <Link to="/dashboard">Share</Link>
+                            <Link to="/">Share</Link>
                     </MenuItem>
                 </Menu>
                 </SidebarContent>
             <SidebarFooter>
-                {/* Footer */}
+                Footer
                 <button>Sign out</button>
             </SidebarFooter>
-        </ProSidebar>
+        </ProSidebar> */}
     </div>
   )
 }
--- src/components/Dashboard/TopNavBar.js
+++ src/components/Dashboard/TopNavBar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {  Link } from "react-router-dom";
 import './TopNavBar.css'
+
 export default function TopNavBar() {
   return (
-    <div className='Nav-container'>
+    <div 
+    className='Nav-container'
+    >
         ICON HERE
-        <li>
+     
+        {/* <li>
             <Link to="/">Home</Link>
         </li>
         <li>
@@ -16,7 +20,48 @@ export default function TopNavBar() {
         </li>
         <li>
             <Link to="/mentor/signin">About Us</Link>
-        </li>
+        </li> */}
     </div>
   )
 }
+
+// ReactDOM.render(
+//   <Layout>
+//     <Sider
+//       breakpoint="lg"
+//       collapsedWidth="0"
+//       onBreakpoint={broken => {
+//         console.log(broken);
+//       }}
+//       onCollapse={(collapsed, type) => {
+//         console.log(collapsed, type);
+//       }}
+//     >
+//       <div className="logo" />
+//       <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+//         <Menu.Item key="1" icon={<UserOutlined />}>
+//           nav 1
+//         </Menu.Item>
+//         <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+//           nav 2
+//         </Menu.Item>
+//         <Menu.Item key="3" icon={<UploadOutlined />}>
+//           nav 3
+//         </Menu.Item>
+//         <Menu.Item key="4" icon={<UserOutlined />}>
+//           nav 4
+//         </Menu.Item>
+//       </Menu>
+//     </Sider>
+//     <Layout>
+//       <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+//       <Content style={{ margin: '24px 16px 0' }}>
+//         <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+//           content
+//         </div>
+//       </Content>
+//       <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+//     </Layout>
+//   </Layout>,
+//   mountNode,
+// );
