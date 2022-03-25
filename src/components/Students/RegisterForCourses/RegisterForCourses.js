f1b651b1252a9af1ce6f444d7a03ceee3823633b
priyasura <priyasura01@gmail.com>
2022-03-25 12:17:26 +0530

    commit

--- src/components/Students/RegisterForCourses/RegisterForCourses.js
+++ src/components/Students/RegisterForCourses/RegisterForCourses.js
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import '../RegisterForCourses/RegisterForCourses.css'
+import img1 from '../../../assets/Images/Courses.png'
+import HeartIcon from "../../../assets/Icons/heart.svg";
 export default function RegisterForCourses() {
   return (
-    <div>plzz register for courses</div>
+    <div >
+      <div className="card1">
+        <div className="Card1_image">
+          <img src={img1}></img>
+        </div>
+        <div className="Card1_Header">
+          <div className="Card1_Headertext"> Python programming</div>
+          <div className="Card1_Headericon"><img src={HeartIcon} style={{width: '30px', height: '20px'}}></img></div>
+        </div>
+        <div className="Card1_secondparttext">-Sunil Kalikayi</div>
+        <div className="Card1_thirdparttext">
+          <div className="Card1_days">• 7days</div>
+          <div className="Card1_time">• 5pm to 7 pm</div>
+        </div>
+        <div className="Card1_button">
+          <button className="Card1_Register">Register Now</button>
+        </div>
+      </div>
+      
+    </div>
   )
 }
