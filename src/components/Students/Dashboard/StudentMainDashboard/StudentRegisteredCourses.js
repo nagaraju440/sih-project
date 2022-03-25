ad63b6bc5f7a6f93ca40383efc82220f46763495
priyasura <priyasura01@gmail.com>
2022-03-25 11:29:29 +0530

    commit

--- src/components/Students/Dashboard/StudentMainDashboard/StudentRegisteredCourses.js
+++ src/components/Students/Dashboard/StudentMainDashboard/StudentRegisteredCourses.js
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import '../StudentMainDashboard/StudentRegisterCourses.css'
+import image from '../../../../assets/Images/studentdashboard.png'
 export default function StudentRegisteredCourses() {
   return (
-    <div>StudentRegisteredCourses</div>
+    <div >
+      <div className="card">
+        <div className="Card_image">
+          <img src={image}></img>
+        </div>
+        <div className="Card_Name"> C programming</div>
+        <div className="Card_secondpart">
+          <div className="Card_text">-By Tejawini</div>
+          <div className="Card_dot">•</div>
+          <div className="Card_time">5pm - 7pm</div>
+        </div>
+        <div className="Card_button">
+          <button className="Card_buttontext" >Join Class</button>
+        </div>
+      </div>
+      
+    </div>
   )
 }
